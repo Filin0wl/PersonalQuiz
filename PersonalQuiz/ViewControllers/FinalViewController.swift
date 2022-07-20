466682a7f280bb8ea5415583351861863dbcfc56
Filin <nastia-dogf@mail.ru>
2022-07-20 13:09:44 +0300

    Remove back button

--- PersonalQuiz/ViewControllers/FinalViewController.swift
+++ PersonalQuiz/ViewControllers/FinalViewController.swift
@@ -12,7 +12,7 @@ class FinalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.navigationItem.setHidesBackButton(true, animated: false)
     }
     
 
